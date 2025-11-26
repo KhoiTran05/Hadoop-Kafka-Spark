f566da0ace384b42d02a229206b68588568fef21
KhoiTran05 <khoitrlmn@gmail.com>
2025-11-26 22:31:59 +0700

    feat: add schema validation with SchemaRegistry to Kafka producer

--- src/ingestion/api/streaming.py
+++ src/ingestion/api/streaming.py
@@ -3,10 +3,12 @@ from utils.rate_limit import RateLimiter
 from utils.api_key import get_api_key
 from utils.api_data_validation import validate_data
 from datetime import datetime, timedelta, timezone, time as dt_time
-from confluent_kafka import Producer
-import requests
+from confluent_kafka import SerializingProducer
+from confluent_kafka.serialization import StringSerializer
+from confluent_kafka.schema_registry import SchemaRegistryClient
+from confluent_kafka.schema_registry.avro import AvroSerializer
 import os
-import json
+import requests
 import time
 
 # Fit in API limitation
@@ -17,44 +19,117 @@ END_TIME = dt_time(14, 0, 0)
 WEATHER_INTERVAL_SECONDS = 5 * 60  
 FOOTBALL_API_INTERVAL_SECONDS = 30
 
-KAFKA_BROKER_URL = os.getenv("KAFKA_BROKER_URL")
 LIVE_FOOTBALL_TOPIC = "football_live"
 HISTORY_FOOTBALL_TOPIC = "football_history"
 WEATHER_TOPIC = "weather"
                 
 class DataIngestionPipeline:
     def __init__(self):
-        self.kafka_producer = self.create_kafka_producer()
+        self.schema_registry_client = SchemaRegistryClient({'url': os.getenv("SCHEMA_REGISTRY_URL")})
+        self.schemas = self.get_avro_schema()
+        self.football_producer = self.create_kafka_producer('football')
+        self.weather_producer = self.create_kafka_producer('weather')
+    
+    def get_avro_schema(self):
+        """Get avro schema for Schema Registry"""
+        football_schema_path = '/opt/airflow/schema/football.avsc'
+        weather_schema_path =  '/opt/airflow/schema/weather.avsc'
+        try:
+            with open(football_schema_path, 'r') as f:
+                football_schema = f.read()
+            with open(weather_schema_path, 'r') as f:
+                weather_schema = f.read()
+            return {
+                'football_schema': football_schema,
+                'weather_schema': weather_schema
+            }
+        except FileNotFoundError as e:
+            logger.error(f"Avro Schema file not found: {str(e)}")
+            raise
+                
+    def create_kafka_producer(self, producer_name):
+        """
+        Create Kafka SerializingProducer with AvroSerializer for value
         
-    def create_kafka_producer(self):
-        """Create Kafka Producer"""
+        Parameters:
+            producer_name (str): Kafka producer name. Must be either 'football' or 'weather'
+                
+        Returns
+            SerializingProducer: Configured Kafka producer ready to produce message
+            
+        Raises:
+            ValueError: If producer_name is not 'football' or 'weather'
+        """
         conf = {
-            'bootstrap.servers': KAFKA_BROKER_URL,
+            'bootstrap.servers': os.getenv("KAFKA_BROKER_URL"),
+            'key.serializer': StringSerializer('utf_8'),
             'compression.type': 'zstd',
             'acks': 'all'
         }
-
-        return Producer(conf)
+        
+        if producer_name == 'football':
+            football_serializer = AvroSerializer(
+                schema_registry_client=self.schema_registry_client,
+                schema_str=self.schemas.get('football_schema')
+            )
+            conf['value.serializer'] = football_serializer
+            
+            producer = SerializingProducer(conf=conf)
+    
+            return producer
+        elif producer_name == 'weather':
+            weather_serializer = AvroSerializer(
+                schema_registry_client=self.schema_registry_client,
+                schema_str=self.schemas.get('weather_schema')
+            )
+            conf['value.serializer'] = weather_serializer
+            
+            producer = SerializingProducer(conf=conf)
+            
+            return producer
+        else:
+            logger.error(f"Producer name {producer_name} not found")
+            raise ValueError(f"Invalid producer name {producer_name}")
     
     def delivery_report(self, err, msg):
-        """Get Kafka message status"""
+        """
+        Callback to report the delivery status of a Kafka message
+        
+        Parameters:
+            err (KafkaError or None): Error info if delivery failed or None if successful
+            msg (Message): Kafka message object
+        
+        Returns:
+            None
+        """
         if err is not None:
             logger.error(f"Failed to deliver message: {err}")
         else:
             dt = datetime.now(timezone.utc).astimezone(timezone(timedelta(hours=7)))
             logger.info(f"{dt.strftime('%H:%M:%S')} - Topic {msg.topic()} delivered.")
         
-    def produce_to_kakfa(self, topic, key, data):
-        """Produce message to Kafka"""
+    def produce_to_kakfa(self, producer, topic, key, value):
+        """
+        Produce a message to Kafka topic
+        
+        Parameters:
+            producer (SerializingProducer): Kafka producer instance 
+            topic (str): Kafka topic to which message will be sent
+            key (int): Kafka message key
+            value (dict): Kafka message value. Must match producer's value serializer schema 
+            
+        Returns:
+            None
+        """
         try:
-            self.kakfa_producer.poll(0)
-            value = json.dumps(data).encode('utf-8')
-            self.kafka_producer.produce(
+            producer.poll(0)
+            producer.produce(
                 topic=topic,
-                key=str(key).encode('utf-8'),
+                key=str(key),
                 value=value,
                 on_delivery=self.delivery_report
             )
+            producer.flush()
         except Exception as e:
             logger.error(f"Error producing to Kafka: {e}")
 
@@ -179,7 +254,7 @@ class DataIngestionPipeline:
             return None
     
     def run_ingestion(self):
-        """Run data ingestion"""
+        """Continuously fetch football and weather data and produce messages to Kafka topics on a schedule"""
         football_last_run = datetime.min.replace(tzinfo=timezone.utc)
         weather_last_run = datetime.min.replace(tzinfo=timezone.utc)
         
@@ -194,17 +269,17 @@ class DataIngestionPipeline:
                     
                     if live_matches:
                         for match in live_matches:
-                            self.produce_to_kakfa(LIVE_FOOTBALL_TOPIC, match.get('id'), match)
+                            self.produce_to_kakfa(self.football_producer, LIVE_FOOTBALL_TOPIC, match.get('id'), match)
                     if finished_matches:
                         for match in finished_matches:
-                            self.produce_to_kakfa(HISTORY_FOOTBALL_TOPIC, match.get('id'), match)
+                            self.produce_to_kakfa(self.football_producer, HISTORY_FOOTBALL_TOPIC, match.get('id'), match)
                     football_last_run = now
                 
                 if START_TIME <= now_time <= END_TIME and (now - weather_last_run).total_seconds() >= WEATHER_INTERVAL_SECONDS:
                     weather_data = self.get_weather_data()
                     if weather_data:
                         for record in weather_data:
-                            self.produce_to_kakfa(WEATHER_TOPIC, record.get('id'), record)
+                            self.produce_to_kakfa(self.weather_producer, WEATHER_TOPIC, record.get('id'), record)
                     weather_last_run = now
                     
                 time.sleep(10)  
@@ -215,8 +290,11 @@ class DataIngestionPipeline:
     def cleanup(self):
         """Cleanup resources"""
         try:
-            self.kafka_producer.flush()
-            self.kafka_producer.close()
+            self.football_producer.flush()
+            self.weather_producer.flush()
+            
+            self.football_producer.close()
+            self.weather_producer.close()
             logger.info("Kafka producer closed")
         except Exception as e:
             logger.error(f"Error during cleanup: {str(e)}")
--- src/streaming/live_football_stream.py
+++ src/streaming/live_football_stream.py
@@ -1,1 +1,13 @@
 from pyspark.sql import SparkSession
+from pyspark.sql.types import StructType, StructField
+
+class LiveMatchesStreamProcessor:
+    def __init__(self):
+        self.spark = self.create_spark_session()
+        
+    def create_spark_session(self):
+        return SparkSession.builder \
+            .appName("LiveMatchesStreamProcessor") \
+            .config("spark.sql.streaming.checkpointLocation", "hdfs://namenode:9000/checkpoints/live_football") \
+            .getOrCreate()
+            
